33c898495035e5aeeb40421fe63a7607f68b8f0f
Ruben Romero Montes <rromerom@redhat.com>
2025-02-13 15:02:56 +0100

    fix: load sa token file
    Signed-off-by: Ruben Romero Montes <rromerom@redhat.com>

--- src/main/java/com/redhat/ecosystemappeng/morpheus/service/jwt/JkwsRequestFilter.java
+++ src/main/java/com/redhat/ecosystemappeng/morpheus/service/jwt/JkwsRequestFilter.java
@@ -1,10 +1,14 @@
 package com.redhat.ecosystemappeng.morpheus.service.jwt;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Optional;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.logging.Logger;
 
 import io.quarkus.arc.Unremovable;
 import io.quarkus.oidc.common.OidcEndpoint;
@@ -17,15 +21,28 @@ import io.vertx.core.http.HttpMethod;
 @Unremovable
 public class JkwsRequestFilter implements OidcRequestFilter {
 
-  @ConfigProperty(name = "openshift.serviceaccount.token")
-  Optional<String> serviceAccountToken;
+  private static final Logger LOGGER = Logger.getLogger(JkwsRequestFilter.class);
+
+  private static final Path SA_TOKEN_PATH = Path.of("/var/run/secrets/kubernetes.io/serviceaccount/token");
+  Optional<String> token = Optional.empty();
+
+  @PostConstruct
+  void loadToken() {
+    if (Files.exists(SA_TOKEN_PATH)) {
+      try {
+        token = Optional.of(Files.readString(SA_TOKEN_PATH));
+      } catch (IOException e) {
+        LOGGER.errorf("Unable to read Service Account Token: %s", SA_TOKEN_PATH.toString(), e);
+      }
+    }
+  }
 
   @Override
   public void filter(OidcRequestContext requestContext) {
     HttpMethod method = requestContext.request().method();
     String uri = requestContext.request().uri();
-    if (method == HttpMethod.GET && uri.endsWith("/jwks") && serviceAccountToken.isPresent()) {
-      requestContext.request().bearerTokenAuthentication(serviceAccountToken.get());
+    if (method == HttpMethod.GET && uri.endsWith("/jwks") && token.isPresent()) {
+      requestContext.request().bearerTokenAuthentication(token.get());
     }
   }
 
