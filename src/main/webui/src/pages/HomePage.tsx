530e699cf09888c647a2cf85e912fd77a8cd798d
rhartuv <rhartuv@redhat.com>
2025-12-03 14:05:38 +0200

    feat:add title and include get_started to home page

--- src/main/webui/src/pages/HomePage.tsx
+++ src/main/webui/src/pages/HomePage.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReportsSummaryCard from '../components/ReportsSummaryCard';
+import { PageSection, Title } from '@patternfly/react-core';
+import GetStartedCard from '../components/GetStartedCard';
 
 /**
  * HomePage component - displays reports summary
  */
 const HomePage: React.FC = () => {
-  return <ReportsSummaryCard />;
+  return (
+    <>
+      <PageSection >
+        <Title headingLevel="h1" size="3xl">
+          Home
+        </Title>
+        <p >
+          Request new analysis and view important system data.
+        </p>
+      </PageSection>
+      <PageSection>
+        <ReportsSummaryCard />
+        <br />
+        <GetStartedCard />
+      </PageSection>
+    </>
+  );
 };
 
 export default HomePage;
