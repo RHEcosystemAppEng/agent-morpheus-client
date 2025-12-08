01eb0fd523d907c285c63733253080e778dcf32f
rhartuv <rhartuv@redhat.com>
2025-12-08 16:58:49 +0200

    feat: add matrix to HomePage

---
+++ src/main/webui/src/components/MatrixCard.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  Grid,
+  GridItem,
+  Title,
+  Stack,
+  Flex,
+  FlexItem,
+  Icon,
+} from "@patternfly/react-core";
+import {
+  CheckCircleIcon,
+  OptimizeIcon,
+  TrendDownIcon,
+} from "@patternfly/react-icons";
+
+interface MatrixStatItemProps {
+  label: string;
+  value: number;
+  icon: React.ReactNode;
+  iconStatus: "custom" | "danger" | "success" | "warning";
+}
+
+const MatrixStatItem: React.FC<MatrixStatItemProps> = ({
+  label,
+  value,
+  icon,
+  iconStatus,
+}) => {
+  return (
+    <GridItem span={4}>
+      <Card ouiaId="BasicCard">
+        <CardBody>
+          <Stack style={{ textAlign: "center" }}>
+            <FlexItem>
+              <Icon status={iconStatus} size="xl">
+                {icon}
+              </Icon>
+            </FlexItem>
+            <FlexItem>
+              <Title headingLevel="h2" size="3xl">
+                {value}
+              </Title>
+            </FlexItem>
+            <FlexItem>{label}</FlexItem>
+          </Stack>
+        </CardBody>
+      </Card>
+    </GridItem>
+  );
+};
+
+const MatrixCard: React.FC = () => {
+  return (
+    <Card>
+      <CardTitle>
+        <Title headingLevel="h2" size="lg">
+          False Positive Detection Matrix
+        </Title>
+      </CardTitle>
+      <CardBody>
+        <Grid hasGutter>
+          <MatrixStatItem
+            label="Placeholder Label 1"
+            value={0}
+            icon={<CheckCircleIcon />}
+            iconStatus="success"
+          />
+          <MatrixStatItem
+            label="Placeholder Label 2"
+            value={0}
+            icon={<TrendDownIcon />}
+            iconStatus="success"
+          />
+          <MatrixStatItem
+            label="Placeholder Label 3"
+            value={0}
+            icon={<OptimizeIcon />}
+            iconStatus="success"
+          />
+        </Grid>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default MatrixCard;
